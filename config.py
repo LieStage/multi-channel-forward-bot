af38c5e3057a3b0122165bc0d0d0f6a9935a671b
LieStage <118051769+LieStage@users.noreply.github.com>
2023-08-17 18:54:15 +0530

    Update and rename sample_config.py to config.py

--- config.py
+++ config.py
@@ -1,10 +1,10 @@
 import os
 
 class Config(object):
-    TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN")
-    APP_ID = int(os.environ.get("APP_ID"))
-    API_HASH = os.environ.get("API_HASH")
-    AUTH_USERS = set(int(x) for x in os.environ.get("AUTH_USERS", "").split())
+    TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN","6434837886:AAEcF3PP64wNIdYPhedI8_IYUXvfuYibVcM")
+    APP_ID = int(os.environ.get("APP_ID","4682685"))
+    API_HASH = os.environ.get("API_HASH","3eba5d471162181b8a3f7f5c0a23c307")
+    AUTH_USERS = set(int(x) for x in os.environ.get("AUTH_USERS", "945284066").split())
     CHANNEL1_ID = os.environ.get("CHANNEL1_ID")
     CHANNEL1_NAME = os.environ.get("CHANNEL1_NAME")
     CHANNEL2_ID = os.environ.get("CHANNEL2_ID")
